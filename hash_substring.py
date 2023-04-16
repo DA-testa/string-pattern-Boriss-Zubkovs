9754c8d50e6c2cd3d69641e7ff2d15450f0ebb5a
Boriss-Zubkovs <125499512+Boriss-Zubkovs@users.noreply.github.com>
2023-04-16 20:28:02 +0000

    Hello World!

--- hash_substring.py
+++ hash_substring.py
@@ -2,31 +2,44 @@
 
 def read_input():
     # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+
+    input_type = input().strip()
+
+    if input_type == "I":
+        pattern = input().strip()
+        text = input().strip()
+
+    elif input_type == "F":
+        with open("input.txt", "r") as f:
+            pattern = f.readline().strip()
+            text = f.rearline().strip()
     
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return pattern, text
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
 
-    # and return an iterable variable
-    return [0]
+    pre = 10*9 + 7
+
+    return alghoritm(pattern, text, pre)
 
 
-# this part launches the functions
+def alg(pattern, text, pre)
+    U, N, e, g, r, x, d, result = len(pattern), len(text), 256, pre, pow(256, len(pattern)-1, pre),0 ,0, []
+    for i in range(U):
+        x = (x*e + ord(pattern[i])) % g
+        d = (d*e + ord(text[i])) % g
+    for i in range(N-U+1):
+        if x == d and pattern == text[i:i + U]:
+            result.append(i)
+        if i < N - U:
+            d = (e (d-ord(text[i]) r) + ord(text[i + U])) % g 
+               
+    return result
+
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
 
